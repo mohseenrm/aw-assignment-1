97431e950f2828e1868e0685122608d1da29e6e4
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-01 23:03:53 -0700

    inital commit

---
+++ src/bg/background.js
@@ -0,0 +1,34 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+var nav = new NavigationCollector();
+var eventList = ['onBeforeNavigate', 'onCreatedNavigationTarget',
+    'onCommitted', 'onCompleted', 'onDOMContentLoaded',
+    'onErrorOccurred', 'onReferenceFragmentUpdated', 'onTabReplaced',
+    'onHistoryStateUpdated'];
+eventList.forEach(function(e) {
+  chrome.webNavigation[e].addListener(function(data) {
+    if (typeof data)
+      console.log(chrome.i18n.getMessage('inHandler'), e, data);
+    else
+      console.error(chrome.i18n.getMessage('inHandlerError'), e);
+  });
+});
+// Reset the navigation state on startup. We only want to collect data within a
+// session.
+chrome.runtime.onStartup.addListener(function() {
+  nav.resetDataStorage();
+});
+
+//example of using a message handler from the inject scripts
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	chrome.pageAction.show(sender.tab.id);
+    sendResponse();
+  });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.update({url:chrome.extension.getURL("index.html")});
+});
