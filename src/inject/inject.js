cdbbb57829d91b7ecf3a6fea32c3881990746049
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-03 14:05:03 -0700

    updating styles

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,4 +10,17 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("JQ: ", $);
 	}
 	}, 10);
+
+	// Toggle Function
+	$('.toggle').click(function(){
+		// Switches the Icon
+		$(this).children('i').toggleClass('fa-pencil');
+		// Switches the forms  
+		$('.form').animate({
+			height: "toggle",
+			'padding-top': 'toggle',
+			'padding-bottom': 'toggle',
+			opacity: "toggle"
+		}, "slow");
+	});
 });
