6a121c6887804f50a3068345c1cc42c41cfa9220
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-03 22:01:05 -0700

    adding ajax requests

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,32 +1,80 @@
-chrome.extension.sendMessage({}, function( response ) {
+chrome.extension.sendMessage( {}, function( response ) {
 	var $loginButton = $( '#login-form--button' );
+	var $loginPassword = $( '#login-form--password' );
+	var $loginUsername = $( '#login-form--username' );
+
 	var $registerButton = $( '#signup-form--button' );
+	var $registerUsername = $( '#signup-form--username' );
+	var $registerPassword = $( '#signup-form--password' );
 
 	// Toggle Function
-	$('.toggle').click(function(){
+	$( '.toggle' ).click( function() {
 		// Switches the Icon
-		$(this).children('i').toggleClass('fa-pencil');
+		$( this ).children( 'i' ).toggleClass( 'fa-pencil' );
 		// Switches the forms  
-		$('.form').animate({
+		$( '.form' ).animate( {
 			height: "toggle",
 			'padding-top': 'toggle',
 			'padding-bottom': 'toggle',
 			opacity: "toggle"
-		}, "slow");
-	});
+		}, "slow" );
+	} );
 
-	$.ajax({
+	$loginButton.click( function( e ) {
+		e.preventDefault();
+		console.log( 'Caught it.' );
+		var payload = {
+			username: $loginUsername.val(),
+			password: $loginPassword.val()
+		};
+		console.log( 'Payload: ', payload );
+		// send ajax request
+		$.ajax( {
+			type: 'POST',
+			url: 'http://localhost:9001/auth/login',
+			data: payload,
+			success: function( result ){
+				console.log( 'result: ', result )
+			},
+			error: function( request, error ){
+				console.log( 'Error: ', error );
+			}
+		} );
+	} );
+
+	$registerButton.click( function( e ) {
+		e.preventDefault();
+		var payload = {
+			username: $registerUsername.val(),
+			password: $registerPassword.val()
+		};
+		console.log( 'Payload: ', payload );
+		// send ajax request
+		$.ajax( {
+			type: 'POST',
+			url: 'http://localhost:9001/auth/create',
+			data: payload,
+			success: function( result ){
+				console.log( 'result: ', result )
+			},
+			error: function( request, error ){
+				console.log( 'Error: ', error );
+			}
+		} );
+	} );
+/* 
+	$.ajax( {
 		type: 'POST',
 		url: 'http://localhost:9001/auth',
 		data: {
 			sample: ' data'
 		},
-		success: function(result){
-			console.log('result: ', result)
+		success: function( result ){
+			console.log( 'result: ', result )
 		},
-		error: function(request, error){
-			console.log('Error: ', error);
+		error: function( request, error ){
+			console.log( 'Error: ', error );
 		}
-	})
+	} ); */
 
-});
+} );
