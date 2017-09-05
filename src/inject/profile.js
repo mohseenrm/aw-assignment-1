161e22d849c96110ddaee3d7effdc2b188e907ae
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-05 13:17:49 -0700

    updating login activity

--- src/inject/profile.js
+++ src/inject/profile.js
@@ -1,19 +1,49 @@
 console.log('loaded profile js: ', chrome);
-chrome.runtime.onMessage.addListener(
-	function(
-		request, 
-		sender, 
-		sendResponse
-	) {
-			console.log('Profile Script: ', request);
-		}
-);
 
+var $activity = $('#activity');
 var $username = $('#username');
+
+var session = {};
 
 chrome.storage.local.get('username', function(user) {
 	/* Retreive username */
 	console.log('Got user: ', user);
 	$username.html(user.username);
+	session.username = user.username;
 	/* Retrieve history */
 });
+
+console.log('runs till here');
+
+var render = function(){
+	renderLastLogin();
+}
+
+var renderLastLogin = function(){
+	if (session.activity && session.activity.length !== 0){
+		$activity.html('Last Login at : ' + session.activity[
+			session.activity.length - 1
+		].timeStamp);
+	}
+}
+
+var retrieveHistory = function(){
+	$.ajax( {
+		type: 'POST',
+		url: 'https://aw-1.herokuapp.com/get/history',
+		data: JSON.stringify({ username: session.username }),
+		contentType: 'application/json; charset=utf-8',
+		dataType: 'json',
+		success: function( result ){
+			console.log( 'get history [client] result: ', result );
+			session.activity = result.events[0].activity;
+			session.events = result.events[0].events;
+			render();
+		},
+		error: function( request, error ){
+			console.log( 'get history Error: ', error );
+		}
+	} );
+}
+
+setTimeout(retrieveHistory, 100);
