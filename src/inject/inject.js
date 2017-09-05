1a1d77af345dbddd7a751e38f6f6c298bd05a626
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-05 00:18:37 -0700

    updating content script

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,17 +1,32 @@
 console.log('loaded content script!');
-chrome.runtime.onMessage.addListener(
-	function(
-		request, 
-		sender, 
-		sendResponse
-	) {
-			console.log('Content Script: ', request);
-			if (request.greeting == "hello") {
-				sendResponse({message: "hi"});
-			}
-		}
-);
+
+var session = {
+	events: []
+};
 
 document.body.addEventListener('click', function(e){
 	console.log('Content Script caught event: ', e);
+	console.log('element: ', e.srcElement.className);
+
+	if (e.srcElement.baseURI.includes('stackoverflow')) {
+		session.events.push({
+			className: e.srcElement.className || '',
+			id: e.srcElement.id || '',
+			timeStamp: (new Date).getTime(),
+			type: e.type,
+		});
+		console.log('session: ', session);
+	}
 }, true);
+/* 
+var updateServer = function(){
+	// check if data exists
+	if (session.events.length !== 0) {
+		// send data to /update/history
+
+		//resetting session
+		session.events = [];
+	}
+}
+
+setInterval(updateServer, 50000); */
