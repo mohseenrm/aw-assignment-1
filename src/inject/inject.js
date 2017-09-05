078b43ae0a1dc15787766786c003e58247a3a050
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-05 00:28:11 -0700

    adding username

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,21 @@
 console.log('loaded content script!');
 
+/* globals */
 var session = {
 	events: []
 };
 
+var userName = '';
+
+// TODO: handle logout, and update username
+chrome.storage.local.get('username', function(user) {
+	/* Retreive username */
+	console.log('Got user: ', user.username);
+	session.username = user.username;
+});
+
 document.body.addEventListener('click', function(e){
 	console.log('Content Script caught event: ', e);
-	console.log('element: ', e.srcElement.className);
 
 	if (e.srcElement.baseURI.includes('stackoverflow')) {
 		session.events.push({
